0765af47a0b53896b5c0706d967b1d8f744f7da8
Itaakara Robertson <iro@netcompany.com>
2022-12-03 10:28:45 +0100

    Cleanup day 3

--- Advent2022/Solutions/Day3.cs
+++ Advent2022/Solutions/Day3.cs
@@ -1,13 +1,11 @@
 ﻿namespace Advent2022.Solutions
 {
     internal static class Day3
-
     {
         public static void GetTaskResults()
         {
             var lines = File.ReadAllLines($"{Directory.GetCurrentDirectory()}\\Inputs\\Day3.txt");
             var totalScorePart1 = 0;
-            var totalScorePart2 = 0;
 
             var elfGroups = new Dictionary<int, List<string>>();
             var groupKey = 0;
@@ -15,7 +13,6 @@
             for (var i = 0; i < lines.Length; i++)
             {
                 var line = lines[i];
-
                 var compartmentSizes = line.Length / 2;
                 var compartment1 = line[..compartmentSizes];
                 var compartment2 = line[compartmentSizes..];
@@ -30,7 +27,7 @@
 
                 totalScorePart1 += dictionary.Values.Sum();
 
-                // Part 2
+                // Part 2 pre-work
                 if (i % 3 == 0)
                 {
                     elfGroups.Add(++groupKey, new List<string>());
@@ -39,26 +36,25 @@
                 elfGroups[groupKey].Add(line);
             }
 
+            // Part 2 (without pre-work done in previous loop)
+            var totalScorePart2 = 0;
             foreach (var elfGroup in elfGroups)
             {
-                var parsedChars = new List<char>();
-                foreach (var letterFromFirstBag in elfGroup.Value[0].Where(letterFromFirstBag => elfGroup.Value[1].Contains(letterFromFirstBag) &&
-                             elfGroup.Value[2].Contains(letterFromFirstBag) &&
-                             !parsedChars.Contains(letterFromFirstBag)))
-                {
-                    totalScorePart2 += letterFromFirstBag.ToElfPriority();
-                    parsedChars.Add(letterFromFirstBag);
-                }
+                if (elfGroup.Value.Count != 3) throw new InvalidOperationException("Elves have to be in groups of three.");
+
+                var groupBadge = elfGroup.Value[0].First(letterFromFirstBag =>
+                    elfGroup.Value[1].Contains(letterFromFirstBag) &&
+                    elfGroup.Value[2].Contains(letterFromFirstBag));
+
+                totalScorePart2 += groupBadge.ToElfPriority();
             }
 
             Console.WriteLine($"Task 1 result is: {totalScorePart1}");
             Console.WriteLine($"Task 2 result is: {totalScorePart2}");
         }
 
-        private static int ToElfPriority(this char character) => char.IsUpper(character) switch
-        {
-            true => character - 64 + 26,
-            _ => character - 96
-        };
-    }
+        // a-z => 1-26
+        // A-Z => 27-52
+        private static int ToElfPriority(this char character) => char.IsUpper(character) ? character - 64 + 26 : character - 96;
+    };
 }
