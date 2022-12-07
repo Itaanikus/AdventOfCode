0bf04d96e8d1b7ef186e4c92d867033d6b4ffa80
Itaakara Robertson <Itaa_10@hotmail.com>
2022-12-07 21:21:02 +0100

    Cleanup day 6

--- Advent2022/Solutions/Day6.cs
+++ Advent2022/Solutions/Day6.cs
@@ -12,37 +12,23 @@ internal static class Day6
         var finalAnswer = true;
         var input = File.ReadAllText(finalAnswer ? InputPath : TestInputPath);
 
-        var task1Result = 0;
-        var task2Result = 0;
+        var taskResult = 0;
 
-        const int QueueLimit = 14; // Change to 14 for part 2...
-        var fourLetterQueue = new Queue<char>(QueueLimit + 1);
+        const int DistinctCharsInARow = 14; // Use value 4|14 for part 1|2
 
-        for (int i = 0; i < input.Length; i++)
+        for (int i = DistinctCharsInARow; i < input.Length; i++)
         {
-            var letter = input[i];
-            fourLetterQueue.Enqueue(letter);
+            var startIndex = i - DistinctCharsInARow;
+            var charsToCheckForUniqueness = input[startIndex..i];
+            var uniqueSet = new HashSet<char>();
 
-            if (fourLetterQueue.Count > QueueLimit)
+            if (charsToCheckForUniqueness.All(uniqueSet.Add))
             {
-                fourLetterQueue.Dequeue();
-            }
-
-            if (fourLetterQueue.Count == QueueLimit && IsUnique(fourLetterQueue))
-            {
-                task1Result = i + 1;
+                taskResult = i;
                 break;
             }
         }
 
-
-        Console.WriteLine($"Task 1 result is: {task1Result}");
-        Console.WriteLine($"Task 2 result is: {task2Result}");
-    }
-
-    private static bool IsUnique(Queue<char> queue)
-    {
-        var diffChecker = new HashSet<char>();
-        return queue.All(diffChecker.Add);
+        Console.WriteLine($"Task result is: {taskResult}");
     }
 }
