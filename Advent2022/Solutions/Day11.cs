736e2df6dd7ce6b7182b25bbef18af59e3efdf9a
Itaakara Robertson <Itaa_10@hotmail.com>
2022-12-10 19:19:15 +0100

    Create Day11.cs

---
+++ Advent2022/Solutions/Day11.cs
@@ -0,0 +1,13 @@
+﻿namespace Advent2022.Solutions;
+
+internal static class Day11
+{
+    private static readonly string InputPath = $"{Directory.GetCurrentDirectory()}\\Inputs\\Day11.txt";
+    private static readonly string ExampleInputPath = $"{Directory.GetCurrentDirectory()}\\Inputs\\Day11_example.txt";
+
+    public static void GetTaskResults()
+    {
+        var useExample = false;
+        var lines = File.ReadAllLines(useExample ? ExampleInputPath : InputPath);
+    }
+}
